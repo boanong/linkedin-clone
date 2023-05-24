f48dc289720b55bb9021e7ec3e41cd3f3b75e4f1
Boanong Joshua <boanongjoshua@gmail.com>
2023-05-24 17:27:19 +0100

    setup topnav

---
+++ src/Components/Topnav/Topnav.tsx
@@ -0,0 +1,15 @@
+"use client";
+import React from "react";
+import { MainDiv } from "@/Components/Organisms/MainDiv";
+
+type Props = {};
+
+function Topnav({}: Props) {
+  return (
+    <MainDiv>
+      <p>hhhh</p>
+    </MainDiv>
+  );
+}
+
+export default Topnav;
