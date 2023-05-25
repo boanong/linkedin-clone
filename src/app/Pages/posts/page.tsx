544cd157dc719a916676f125a967016125fe8ea3
NNVG <g696607013@gmail.com>
2023-05-25 10:58:32 +0100

    Setting up the post page

---
+++ src/app/Pages/posts/page.tsx
@@ -0,0 +1,15 @@
+'use client'
+import React from 'react';
+import { Main, NavBar } from '@/Components/Organisms/Organisms';
+
+type Props = {}
+
+function Login({}: Props) {
+  return (
+    <Main>
+      hello
+    </Main>
+  )
+}
+
+export default Login
