c0154c65bd6a48096b83cb14b53d062f4bd27b29
Kiven Alaric <elmozaro3@gmail.com>
2023-05-26 04:58:59 -0500

    Profile icon rename

--- src/Components/Atoms/ProfileIcon.tsx
+++ src/Components/Atoms/ProfileIcon.tsx
@@ -3,7 +3,7 @@ import { VscAccount } from 'react-icons/vsc';
 
 
 
-export const PhotoIcon = styled(VscAccount)`
+export const ProfileIcon = styled(VscAccount)`
   font-size: 2.6rem;
   color: #666666;
   @media only screen and (min-width: 768px) {
