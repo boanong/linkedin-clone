bf7207f5231cf0aceacb90fd56246d8cb0d2353f
Kiven Alaric <elmozaro3@gmail.com>
2023-05-26 12:09:15 -0500

    addition of components of feed page

--- src/app/Pages/Feed/page.tsx
+++ src/app/Pages/Feed/page.tsx
@@ -1,23 +1,16 @@
-<<<<<<< HEAD
 "use client";
 import React from 'react'
 import FeedUserSec from '@/Components/FeedUser/FeedUser'
 import { FeedInnerMain } from '@/Components/Organisms/FeedInnerMain'
 import styled from '@emotion/styled'
-
-=======
-'use client'
-import styled from '@emotion/styled'
-import React from 'react'
 import Topnav from '@/Components/Topnav/Topnav';
->>>>>>> e5086ed5a799de2b0fbca115bb79548b158e579a
+
 
 type Props = {}
 
 const FeedMain = styled.div`
   display: flex;
   flex-direction: column;
-<<<<<<< HEAD
   align-items: center;
   margin: 0;
   padding: 0;
@@ -25,22 +18,14 @@ const FeedMain = styled.div`
   height: fit-content;
   min-height: 100vh;
 `
-=======
-  padding: 0;
-  margin: 0;
-`;
->>>>>>> e5086ed5a799de2b0fbca115bb79548b158e579a
 
 function feed({}: Props) {
   return (
     <FeedMain>
-<<<<<<< HEAD
+      <Topnav />
       <FeedInnerMain>
         <FeedUserSec />
       </FeedInnerMain>
-=======
-      <Topnav />
->>>>>>> e5086ed5a799de2b0fbca115bb79548b158e579a
     </FeedMain>
   )
 }
