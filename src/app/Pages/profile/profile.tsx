17a82f4424326b62fc1d9b62f6bbbda4ba94f575
Kiven Alaric <elmozaro3@gmail.com>
2023-05-26 04:55:49 -0500

    File additions

---
+++ src/app/Pages/profile/profile.tsx
@@ -0,0 +1,16 @@
+import styled from '@emotion/styled'
+import React from 'react'
+
+type Props = {}
+
+const ProfileMain = styled.div`
+    width
+`;
+
+function profile({}: Props) {
+  return (
+    <div>profile</div>
+  )
+}
+
+export default profile
