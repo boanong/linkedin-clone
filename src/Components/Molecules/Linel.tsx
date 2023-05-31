ee12e30642d95533ccad88ee09392b7c51b6a416
Kiven Alaric <elmozaro3@gmail.com>
2023-05-31 05:02:12 -0500

    edit on loader component

--- src/Components/Molecules/Linel.tsx
+++ src/Components/Molecules/Linel.tsx
@@ -3,11 +3,10 @@ import styled from "@emotion/styled";
 export const Linel = styled.div`
   height: 3px;
   width: 100%;
-  max-width: 200px;
+  max-width: 280px;
   background: #e8e8e8;
   border-radius: 2px;
-  position: absolute;
+  margin: auto;
   transform: translate(-50%, -50%);
   overflow: hidden;
-  margin-top: 30%;
 `;
