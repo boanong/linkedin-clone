a4fc67c2b03d11edbb46ed14853fbf28e447c11e
11 Rebase Academy <11rebaseacademy@11s-iMac.local>
2023-06-05 17:42:21 +0100

    Fixed the postsSection

--- src/app/Pages/feed/page.tsx
+++ src/app/Pages/feed/page.tsx
@@ -8,6 +8,7 @@ import { PostSecHolder } from "@/Components/Organisms/PostSecHolder";
 import Post from "@/Components/Post/Post";
 import FeedR from "@/Components/FeedRight/FeedR";
 import AuthGaurd from "@/hoc/AuthGaurd";
+import { PostsSection } from "@/Components/PostSection/PostsSection";
 
 type Props = { userData: any };
 
@@ -30,6 +31,7 @@ function Feed({ userData }: Props) {
         <FeedUserSec userData={userData} />
         <PostSecHolder>
           <Post userData={userData} />
+          <PostsSection />
         </PostSecHolder>
         {/* <FeedR /> */}
       </FeedInnerMain>
