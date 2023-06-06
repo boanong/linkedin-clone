4e14fa4f2e179d3c44b0d36e6b481286e59af748
Boanong Joshua <boanongjoshua@gmail.com>
2023-06-06 11:37:03 +0100

    Deleted firebase files

--- src/Components/FeedUser/FeedUser.tsx
+++ src/Components/FeedUser/FeedUser.tsx
@@ -15,7 +15,7 @@ import {
   FeedUserP1U,
   FeedUserTxtH,
 } from "../Molecules/FeedUserDivs";
-import { PostSection, Header, ProfilePicture, JobTitle, PostText, PostImage, OptionsSection } from "../postSection/PostSection";
+import { PostSection, Header, ProfilePicture, JobTitle, PostText, PostImage, OptionsSection } from "../Molecules/PostSection";
 
 
 type Props = { userData: any };
@@ -64,24 +64,6 @@ export default function FeedUserSec({ userData }: Props) {
           <FeedUserP1U>My Items</FeedUserP1U>
         </FeedUserTxtH>
       </FeedUserDiv1>
-
-
-      <PostSection>
-        <Header>
-          <ProfilePicture
-            src="https://media.istockphoto.com/id/1286810719/photo/smiling-cheerful-young-adult-african-american-ethnicity-man-looking-at-camera-standing-at.jpg?s=612x612&w=0&k=20&c=b9sWYITIZ_yjXB3m-Xftj-latPXQDhb5Roa0pA0JaNY="
-            alt="Profile Picture"
-          />
-          <JobTitle>UI/UX Designer || Full stack Developer</JobTitle>
-        </Header>
-        <PostText>Rebase Academy offers comprehensive and personalized programming education for students of all skill levels. With experienced instructors and a flexible curriculum, students can learn at their own pace and gain the skills needed to become successful developers. From web development to machine learning, Rebase Academy provides a top-notch education for the next generation of programmers.</PostText>
-        <PostImage src="https://cdn.pixabay.com/photo/2016/11/19/15/32/laptop-1839876_1280.jpg" alt="Post Image" />
-       
-        <OptionsSection>
-        </OptionsSection> 
-
-      </PostSection> 
-
     </FeedUser>
   );
 }
