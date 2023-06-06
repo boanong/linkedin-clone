51bf94b46b95f1d0391c38349365aa44963defdf
Kiven Alaric <elmozaro3@gmail.com>
2023-06-06 05:55:09 -0500

    created like atom and started function

--- src/Components/Atoms/LikeIcon.tsx
+++ src/Components/Atoms/LikeIcon.tsx
@@ -1,70 +1,80 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 import styled from "@emotion/styled";
-import { SlLike } from "react-icons/sl"
-import { LikeIcon2, Celebrate, SupportIcon, HeartIcon, InsightIcon, FunnyIcon } from './Reaction';
+import { SlLike } from "react-icons/sl";
+import {
+  LikeIcon2,
+  Celebrate,
+  SupportIcon,
+  HeartIcon,
+  InsightIcon,
+  FunnyIcon,
+} from "./Reaction";
 
 export const LikeIcon = styled(SlLike)`
-    color: #666666;
-    font-size: 1rem;
-    font-weight: 300;
-    @media only screen and (min-width: 770px) {
-        font-weight: 600;
-        font-size: 1.2rem;
-    }
+  color: #666666;
+  font-size: 1rem;
+  font-weight: 300;
+  @media only screen and (min-width: 770px) {
+    font-weight: 600;
+    font-size: 1.2rem;
+  }
 `;
 
 export const LikeP = styled.p`
-    color: #666666;
-    font-size: 1rem;
-    font-weight: 300;
-    @media only screen and (min-width: 770px) {
-        font-weight: 600;
-        font-size: 1.2rem;
-    }
+  color: #666666;
+  font-size: 1rem;
+  font-weight: 300;
+  @media only screen and (min-width: 770px) {
+    font-weight: 600;
+    font-size: 1.2rem;
+  }
 `;
 
 const LikeHolder = styled.div`
-    display: flex;
-    align-items: center;
-    padding: 3px 10px;
-    gap: 10px;
-    @media only screen and (min-width: 770px)  {
-        padding: 5px 25px;
-        gap: 15px;
-        &:hover + ReactionsHolder {
-            display: block;
-            background-color: #acacac;;
-        }
+  display: flex;
+  align-items: center;
+  padding: 3px 10px;
+  gap: 10px;
+  @media only screen and (min-width: 770px) {
+    padding: 5px 25px;
+    gap: 15px;
+    &:hover + ReactionsHolder {
+      display: block;
+      background-color: #acacac;
     }
+  }
 `;
 
 const ReactionsHolder = styled.div`
-    display: none;
-    align-items: center;
-    gap: 8px;
-    padding: 2px 8px;
+  display: none;
+  align-items: center;
+  gap: 8px;
+  padding: 2px 8px;
 `;
 
-
-
-type Props = {}
+type Props = {};
 
 function LikeButton({}: Props) {
-    const [likes, setLikes] = useState()
-    const [likeCount, setLikeCount] = useState()
-    const handleLike = (userId:any, postId:any ) => {
-
-    }
+  const [likes, setLikes] = useState();
+  const [likeCount, setLikeCount] = useState();
+  const handleLike = (userId: any, postId: any) => {};
 
   return (
-  <>
-  <ReactionsHolder><LikeIcon2 onClick={() => handleLike()} /><Celebrate /><SupportIcon /><HeartIcon /><InsightIcon /><FunnyIcon /></ReactionsHolder>
-  <LikeHolder>
+    <>
+      <ReactionsHolder>
+        <LikeIcon2 onClick={() => handleLike()} />
+        <Celebrate />
+        <SupportIcon />
+        <HeartIcon />
+        <InsightIcon />
+        <FunnyIcon />
+      </ReactionsHolder>
+      <LikeHolder>
         <LikeIcon />
         <LikeP>Like</LikeP>
-    </LikeHolder>
-  </>
-  )
+      </LikeHolder>
+    </>
+  );
 }
 
-export default LikeButton
+export default LikeButton;
